532c75365a92fc2a61fb4bb92e66c6c96818a221
jfdalmeida <jfdalmeida@fi103-13.admin.wpi.edu>
2020-01-30 10:34:50 -0500

    Goat and Gear ready to print
    goat and gear subtraceed

--- CadGenerator.groovy
+++ CadGenerator.groovy
@@ -6,10 +6,10 @@ def bevelGears = ScriptingEngine.gitScriptRun(
             "https://github.com/madhephaestus/GearGenerator.git", // git location of the library
             "bevelGear.groovy" , // file to load
             // Parameters passed to the funcetion
-            [	  36,// Number of teeth gear a
-	            18,// Number of teeth gear b
+            [	  46,// Number of teeth gear a
+	            24,// Number of teeth gear b
 	            6,// thickness of gear A
-	            6,// gear pitch in arc length mm
+	            4,// gear pitch in arc length mm
 	           90,// shaft angle, can be from 0 to 100 degrees
 	            0// helical angle, only used for 0 degree bevels
             ]
@@ -41,6 +41,10 @@ for(int i=0; i<=numGears; i++){
 
 def topGear = bevelGears.get(0).roty(180).movez(2*bevelGears.get(3))
 
-CSG goat  = Vitamins.get(goatFile).scale(0.5).toZMin().movez(topGear.getMaxZ())
+CSG goat  = Vitamins.get(goatFile).scale(0.5).toZMin().movez(topGear.getMaxZ()-1).movex(-5)
+
+topGear = topGear.difference(goat)
+topGear.setName("top gear")
+goat.setName('Goat')
 
 return [bevelGears.get(0), topGear, roundGears, goat]
