e5b706a0cf3e3db5c6c86114ab9632f3fc2bb0d5
jfdalmeida <jfdalmeida@fi103-13.admin.wpi.edu>
2020-01-30 10:15:18 -0500

    gears added in array
    gears around are generated through an array

--- CadGenerator.groovy
+++ CadGenerator.groovy
@@ -6,10 +6,10 @@ def bevelGears = ScriptingEngine.gitScriptRun(
             "https://github.com/madhephaestus/GearGenerator.git", // git location of the library
             "bevelGear.groovy" , // file to load
             // Parameters passed to the funcetion
-            [	  34,// Number of teeth gear a
-	            24,// Number of teeth gear b
+            [	  36,// Number of teeth gear a
+	            18,// Number of teeth gear b
 	            6,// thickness of gear A
-	            computeGearPitch(26.15,24),// gear pitch in arc length mm
+	            6,// gear pitch in arc length mm
 	           90,// shaft angle, can be from 0 to 100 degrees
 	            0// helical angle, only used for 0 degree bevels
             ]
@@ -21,14 +21,17 @@ println "Bevel angle " + bevelGears.get(4)
 println "Bevel tooth face length " + bevelGears.get(5)
 println "Gear B computed thickness " + bevelGears.get(6)
 // return the CSG parts
-bevelgears roundGears[];
 
-int numGears = 3;
+int numGears = 6;
 
-for(i=0, i<=numGears, i++){
-	roundGears[i] = bevelGears.get(1).rotz(90 * i);
+def roundGears=[];
+
+
+
+for(int i=0; i<=numGears; i++){
+	roundGears.add( bevelGears.get(1).rotz(90 * i))
 }
 
-def topGear = bevelGears.get(0).roty(180).movez(2*bevelGears.get(3));
+def topGear = bevelGears.get(0).roty(180).movez(2*bevelGears.get(3))
 
-return [bevelGears.get(0), topGear]
+return [bevelGears.get(0), topGear,roundGears]
