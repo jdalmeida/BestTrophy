24dd537522fefca9ae3ee0df8cc84df7ab1dcdef
Jesse d'Almeida <37381332+jdalmeida@users.noreply.github.com>
2020-02-03 15:44:08 -0500

    Bolts and Holes added
    bolts to hold the planet gears are added and subtracted from the gears for the holes

--- CadGenerator.groovy
+++ CadGenerator.groovy
@@ -8,44 +8,68 @@ def bevelGears = ScriptingEngine.gitScriptRun(
             // Parameters passed to the funcetion
             [	  46,// Number of teeth gear a
 	            24,// Number of teeth gear b
-	            6,// thickness of gear A
-	            4,// gear pitch in arc length mm
+	            5,// thickness of gear A
+	            6,// gear pitch in arc length mm
 	           90,// shaft angle, can be from 0 to 100 degrees
 	            0// helical angle, only used for 0 degree bevels
             ]
             )
 //Print parameters returned by the script
-println "Bevel gear axle center to center " + bevelGears.get(2)
-println "Bevel gear axle Height " + bevelGears.get(3)
+println "Bevel gear axle center to center " + bevelGears.get(2) // radius of flat gear
+println "Bevel gear axle Height " + bevelGears.get(3)	// radius of perp gear
 println "Bevel angle " + bevelGears.get(4)
 println "Bevel tooth face length " + bevelGears.get(5)
 println "Gear B computed thickness " + bevelGears.get(6)
 // return the CSG parts
-
+           
 File goatFile = ScriptingEngine.fileFromGit(
 	"https://github.com/jdalmeida/BestTrophy.git",
 	"goat.stl");
 
 
-
-int numGears = 6;
+// Generate Gears
+int numGears = 6
+int numShafts = numGears/2
 
 def roundGears=[];
+def bolts=[]
 
 double angle = 360/numGears
 
+def boltsize = 20
+LengthParameter boltLength = new LengthParameter("Bolt Length",10,[180,10])
+boltLength.setMM(boltsize)
+CSG m5Bolt = Vitamins.get("capScrew", "M5").movez(boltsize).roty(90).movez(bevelGears.get(3)).movex(-1*bevelGears.get(2) + boltsize + 5)
 
+
+// add gears around circle
 for(int i=0; i<=numGears; i++){
-	roundGears.add( bevelGears.get(1).rotz(angle * i))
+	CSG gear = bevelGears.get(1).rotz(angle * i)
+	
+	bolts.add(Vitamins.get("capScrew", "M5").
+		movez(boltsize).roty(90).
+		movez(bevelGears.get(3)).movex(-1*bevelGears.get(2) + boltsize + 5).
+		rotz(angle*i))
+
+	roundGears.add(gear.difference(bolts.get(i)))
+	roundGears.get(i).setName("Planet Gears")
 }
 
+// Top gear 
 def topGear = bevelGears.get(0).roty(180).movez(2*bevelGears.get(3))
 
-CSG goat  = Vitamins.get(goatFile).scale(0.5).toZMin().movez(topGear.getMaxZ()-1).movex(-5)
+CSG topBolt = m5bolt.movez(topGear.
 
+// Goat Statue
+CSG goat  = Vitamins.get(goatFile).scale(0.7).toZMin().movez(topGear.getMaxZ()-1).movex(-5)
+
+// Notch in top Gear
 topGear = topGear.difference(goat)
+
 topGear.setName("top gear")
 goat.setName('Goat')
-CSG vitamin_roundMotor_WPI_gb37y3530_50en = Vitamins.get("roundMotor", "WPI-gb37y3530-50en")
 
-return [bevelGears.get(0), topGear, roundGears, goat,vitamin_roundMotor_WPI_gb37y3530_50en]
+// Make Motor
+CSG motor = Vitamins.get("roundMotor", "WPI-gb37y3530-50en")
+
+return [bevelGears.get(0), topGear, roundGears, bolts]
